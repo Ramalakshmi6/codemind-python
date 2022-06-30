02527ed5eae1729d420373948e6ec1253127e275
Rama lakshmi <98380738+Ramalakshmi6@users.noreply.github.com>
2022-06-30 22:23:13 +0530

    n=int(input()) c=0 x=0 y=0 for i in range(2,n):     if n%i==0:         c+=1 if c==0:     while n>0:         d=n%10         n=n//10         for i in range(2,d):             if d%i==0:                 x+=1         if d==1:             y+=1 if c==0 and x==0 and y==0:     print('Mega Prime') else:     print('Not Mega Prime')

--- mega_prime.py
+++ mega_prime.py
@@ -1,26 +1,20 @@
-def is_prime(n):
-    c=0
-    flag=1
-    for i in range(1,n+1):
-        if n%i==0:
-            c+=1
-    if c==2:
-        return flag
-    else:
-        return 0
 n=int(input())
-if(is_prime(n)):
-    r=0
-    v=0
-    while(n>0):
+c=0
+x=0
+y=0
+for i in range(2,n):
+    if n%i==0:
+        c+=1
+if c==0:
+    while n>0:
         d=n%10
-        if(is_prime(d)):
-            r+=1
-        v+=1
         n=n//10
-    if v==r:
-        print("Mega Prime")
-    else:
-        print("Not Mega Prime")
+        for i in range(2,d):
+            if d%i==0:
+                x+=1
+        if d==1:
+            y+=1
+if c==0 and x==0 and y==0:
+    print('Mega Prime')
 else:
-    print("Not Mega Prime")
+    print('Not Mega Prime')
