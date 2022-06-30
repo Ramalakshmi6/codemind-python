cffacd3cb1485f244adbf91322145ddae9161685
Rama lakshmi <98380738+Ramalakshmi6@users.noreply.github.com>
2022-06-30 22:33:07 +0530

    def is_ugly(num):         if num == 0:             return False         for i in [2, 3, 5]:             while num % i == 0:                 num /= i         if(num==1):             return 1         else:             return 0 a=int(input()) if(is_ugly(a)):     print("Ugly Number") else:     print("Not Ugly Number")

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,15 +1,15 @@
-num=int(input())
-x=0
-while num!=1:
-    if num%2==0:
-        num=num//2
-    elif num%3==0:
-        num=num//3
-    elif num%5==0:
-        num=num//5
-    else:
-        print('Not Ugly Number')
-        x=x+1
-        break
-if(x==0):
-    print('Ugly Number')
+def is_ugly(num):
+        if num == 0:
+            return False
+        for i in [2, 3, 5]:
+            while num % i == 0:
+                num /= i
+        if(num==1):
+            return 1
+        else:
+            return 0
+a=int(input())
+if(is_ugly(a)):
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
